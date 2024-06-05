a5916d02f0a8f80e370f8f8b6268cfb304376c58
Ayman Haseeb <114000374+Aymmaann@users.noreply.github.com>
2024-06-05 19:05:27 +0530

    Update script.js

--- FAQ accordion/script.js
+++ FAQ accordion/script.js
@@ -4,11 +4,11 @@ clickIcons.forEach((icon) => {
     icon.addEventListener("click", () => {
         let answer = icon.parentElement.nextElementSibling
         if(icon.src.includes("icon-plus")) {
-            icon.src = "./assets/images/icon-minus.svg"
+            icon.src = "./images/icon-minus.svg"
             answer.style.display = "block"
         }
         else {
-            icon.src = "./assets/images/icon-plus.svg"
+            icon.src = "./images/icon-plus.svg"
             answer.style.display = "none"
         }
     })

7a0cdeed6da59be7bff16be9311dc7c7dbd8131d
Ayman Haseeb <114000374+Aymmaann@users.noreply.github.com>
2024-06-17 11:59:39 +0530

    Add script.js

---
+++ Advice generator app/script.js
@@ -0,0 +1,14 @@
+const adviceNumber = document.querySelector(".advice-number")
+const adviceQuote = document.querySelector(".advice")
+const toggleButton = document.querySelector(".toggle-button")
+
+async function randomAdvice() {
+    let fetchedAdvice = await fetch("https://api.adviceslip.com/advice").then((res) =>
+        res.json()
+    )
+    const {id, advice} = fetchedAdvice.slip
+    adviceNumber.textContent = id
+    adviceQuote.textContent = `"${advice}` 
+}
+
+toggleButton.addEventListener("click", randomAdvice)
